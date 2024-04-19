4cacf93a653446541ed731d7d19b92b6bee39c0f
Lal George <lalg206@yahoo.com>
2024-04-19 12:15:33 -0400

    added buy and hold

--- code/Python/universal/combinedPlots.py
+++ code/Python/universal/combinedPlots.py
@@ -9,21 +9,22 @@ def readFile(file, sortBy = "date") :
     df = pd.read_csv(file)
     return df.sort_values(by = sortBy)
 
-def plot(ax, df, dt, ynames) :
+def plot(ylabel, ax, df, dt, ynames) :
     df[dt] = pd.to_datetime(df[dt])
     x = df[dt]
     ys = [(yname, df[yname]) for yname in ynames]
     for (yname, y) in ys :
        ax.plot(x, y, '-', label=yname)
        
-    ax.set_title("Portfolios Wealth")
+#    ax.set_title(title)
     ax.set_xlabel("Date")
-    ax.set_ylabel("Wealth")
+    ax.set_ylabel(ylabel)
     
     dateForm = mdates.DateFormatter("%Y-%m")
     ax.xaxis.set_major_formatter(dateForm)
     ax.legend()
-
+#    plt.gcf().autofmt_xdate()
+    
 def driver(envName, portfolioName) :
     env = Env(envName)
 
@@ -32,14 +33,14 @@ def driver(envName, portfolioName) :
     weightsDf = readFile(weightsFile, "date")
     wealthDf = readFile(wealthFile, "date")
 
-    fig, axs = plt.subplots(2, figsize=(10,7))
+    fig, axs = plt.subplots(2, figsize=(9, 8))
     fig.suptitle("Universal Portfolio Analysis")
     wlt_ynames = list(wealthDf.columns)
     wlt_ynames.remove("date")
     wts_ynames = list(weightsDf.columns)
     wts_ynames.remove("date")
-    plot(axs[0], wealthDf, "date", wlt_ynames)
-    plot(axs[1], weightsDf, "date", wts_ynames)
+    plot("Wealth", axs[0], wealthDf, "date", wlt_ynames)
+    plot("Weights", axs[1], weightsDf, "date", wts_ynames)
     plt.show()
 
 if __name__ == '__main__' :
